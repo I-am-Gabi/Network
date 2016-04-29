daf2ac9c149aa556d2616d5c722d0847395102ad
I-am-Gabi <gabicavalcantesilva@gmail.com>
2016-04-30 00:29:50 +0200

    simple file to simulate a database

---
+++ src/communication/response/ShowIdeas.java
@@ -0,0 +1,7 @@
+package communication.response;
+
+/**
+ * @version 29/04/16.
+ */
+public class ShowIdeas {
+}
---
+++ src/util/DataBase.java
@@ -0,0 +1,7 @@
+package util;
+
+/**
+ * @version 29/04/16.
+ */
+public class DataBase {
+}
